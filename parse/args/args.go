44c4bbb0f5b93b4ec2e83a4a93eff0349528b702
Sergey Kamardin <gobwas@gmail.com>
2020-09-05 18:58:22 +0300

    parse/args: ability to get non-option args

--- parse/args/args.go
+++ parse/args/args.go
@@ -3,31 +3,129 @@ package args
 import (
 	"context"
 	"flag"
-	"io/ioutil"
+	"fmt"
+	"strings"
 
 	"github.com/gobwas/flagutil/parse"
 )
 
 type Parser struct {
 	Args []string
+
+	fs    parse.FlagSet
+	pos   int
+	name  string
+	value string
+	err   error
 }
 
 func (p *Parser) Parse(_ context.Context, fs parse.FlagSet) error {
-	dup := flag.NewFlagSet("", flag.ContinueOnError)
-	dup.SetOutput(ioutil.Discard)
-	dup.Usage = func() {}
+	p.reset(fs)
+	for p.next() {
+		if fs.Lookup(p.name) == nil && (p.name == "help" || p.name == "h") {
+			return flag.ErrHelp
+		}
+		if err := fs.Set(p.name, p.value); err != nil {
+			return err
+		}
+	}
+	return p.err
+}
 
-	// To get rid of "flag provided but not defined" error by flag package we
-	// need to visit all flags here, even those which provided already.
-	fs.VisitAll(func(f *flag.Flag) {
-		dup.Var(f.Value, f.Name, f.Usage)
-	})
-
-	return dup.Parse(p.Args)
+func (p *Parser) NonFlagArgs() []string {
+	if p.pos < len(p.Args) {
+		return p.Args[p.pos:]
+	}
+	return nil
 }
 
 func (p *Parser) Name(_ context.Context, fs parse.FlagSet) (func(*flag.Flag, func(string)), error) {
 	return func(f *flag.Flag, it func(string)) {
 		it("-" + f.Name)
 	}, nil
+}
+
+func (p *Parser) reset(fs parse.FlagSet) {
+	p.fs = fs
+	p.pos = 0
+	p.err = nil
+}
+
+func (p *Parser) next() bool {
+	if p.err != nil {
+		return false
+	}
+	if p.pos >= len(p.Args) {
+		return false
+	}
+	s := p.Args[p.pos]
+	if len(s) < 2 || s[0] != '-' {
+		return false
+	}
+	p.pos++
+
+	minuses := 1
+	if s[1] == '-' {
+		minuses = 2
+		if len(s) == minuses { // "--" terminates all flags.
+			return false
+		}
+	}
+	name := s[minuses:]
+	if name[0] == '-' || name[0] == '=' {
+		p.fail("bad flag syntax: %s", s)
+		return false
+	}
+
+	name, value, hasValue := split(name, '=')
+	if !hasValue && p.pos < len(p.Args) {
+		value = p.Args[p.pos]
+		if len(value) > 0 && value[0] != '-' {
+			hasValue = true
+			p.pos++
+		}
+	}
+	if !hasValue && p.isBoolFlag(name) {
+		value = "true"
+		hasValue = true
+	}
+	if !hasValue {
+		p.fail("flag needs an argument: -%s", name)
+		return false
+	}
+
+	p.name = name
+	p.value = value
+	return true
+}
+
+func (p *Parser) isBoolFlag(name string) bool {
+	f := p.fs.Lookup(name)
+	if f == nil && (name == "help" || name == "h") {
+		// Special case for help message request.
+		return true
+	}
+	if f == nil {
+		return false
+	}
+	return isBoolFlag(f)
+}
+
+func (p *Parser) fail(f string, args ...interface{}) {
+	p.err = fmt.Errorf(f, args...)
+}
+
+func split(s string, sep byte) (a, b string, ok bool) {
+	i := strings.IndexByte(s, sep)
+	if i == -1 {
+		return s, "", false
+	}
+	return s[:i], s[i+1:], true
+}
+
+func isBoolFlag(f *flag.Flag) bool {
+	x, ok := f.Value.(interface {
+		IsBoolFlag() bool
+	})
+	return ok && x.IsBoolFlag()
 }
--- parse/args/args_test.go
+++ parse/args/args_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 
 	"github.com/gobwas/flagutil"
 	"github.com/gobwas/flagutil/parse"
@@ -11,6 +12,121 @@ import (
 )
 
 var _ flagutil.Printer = new(Parser)
+
+func TestFlagsParseArgs(t *testing.T) {
+	for _, test := range []struct {
+		name     string
+		flags    map[string]bool
+		args     []string
+		expPairs [][2]string
+		expArgs  []string
+		err      bool
+	}{
+		{
+			name: "basic",
+			flags: map[string]bool{
+				"a":   true,
+				"b":   true,
+				"c":   true,
+				"foo": false,
+				"bar": false,
+			},
+			args: []string{
+				"-a",
+				"-b", "true",
+				"-c", "false",
+				"-foo", "value",
+				"--bar", "value",
+				"arg1", "arg2", "arg3",
+			},
+			expPairs: [][2]string{
+				{"a", "true"},
+				{"b", "true"},
+				{"c", "false"},
+				{"foo", "value"},
+				{"bar", "value"},
+			},
+			expArgs: []string{
+				"arg1", "arg2", "arg3",
+			},
+		},
+		{
+			name: "flags termination",
+			flags: map[string]bool{
+				"param": false,
+			},
+			args: []string{
+				"--param", "value",
+				"--",
+				"arg1", "arg2", "arg3",
+			},
+			expPairs: [][2]string{
+				{"param", "value"},
+			},
+			expArgs: []string{
+				"arg1", "arg2", "arg3",
+			},
+		},
+		{
+			name: "basic error",
+			flags: map[string]bool{
+				"param": false,
+			},
+			args: []string{
+				"-param",
+			},
+			err: true,
+		},
+		{
+			name: "basic error",
+			flags: map[string]bool{
+				"param": false,
+			},
+			args: []string{
+				"--param",
+			},
+			err: true,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			fs := testutil.StubFlagSet{
+				IgnoreUndefined: true,
+			}
+			for name, isBool := range test.flags {
+				if isBool {
+					fs.AddBoolFlag(name, false)
+				} else {
+					fs.AddFlag(name, "")
+				}
+			}
+			p := Parser{
+				Args: test.args,
+			}
+			err := p.Parse(context.Background(), &fs)
+			if !test.err && err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if test.err && err == nil {
+				t.Fatalf("want error; got nothing")
+			}
+			if test.err {
+				return
+			}
+			if exp, act := test.expPairs, fs.Pairs(); !cmp.Equal(act, exp) {
+				t.Errorf(
+					"unexpected set pairs:\n%s",
+					cmp.Diff(exp, act),
+				)
+			}
+			if exp, act := test.expArgs, p.NonFlagArgs(); !cmp.Equal(act, exp) {
+				t.Errorf(
+					"unexpected non-flag arguments:\n%s",
+					cmp.Diff(exp, act),
+				)
+			}
+		})
+	}
+}
 
 func TestArgs(t *testing.T) {
 	testutil.TestParser(t, func(values testutil.Values, fs parse.FlagSet) error {
